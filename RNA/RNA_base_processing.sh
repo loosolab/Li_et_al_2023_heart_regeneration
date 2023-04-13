1c77e4c669430280c11ddd2170be8705952f13b0
ckuenne <ckuenne@users.noreply.github.com>
2023-04-13 14:07:04 +0200

    Update RNA_base_processing.sh

--- RNA/RNA_base_processing.sh
+++ RNA/RNA_base_processing.sh
@@ -21,28 +21,11 @@ java -jar trimmomatic.jar SE -threads 8 ./raw/${sample_prefix}_R1.fastq ./trim/$
 ## Mapping: STAR
 STAR --genomeDir /mnt/flatfiles/organisms/new_organism/mus_musculus/101/index_star --runThreadN 16 --readFilesIn ./trim/${sample_prefix}_R1_trim.fastq --outReadsUnmapped Fastx --outFileNamePrefix ./star/${sample_prefix} --outSAMstrandField intronMotif --outSAMtype BAM Unsorted --genomeLoad LoadAndKeep --outFilterMismatchNoverLmax 0.1 --outFilterMatchNmin 20 --alignIntronMax 200000 --alignMatesGapMax 2000 --alignEndsProtrude 10 ConcordantPair --outMultimapperOrder Random --limitOutSAMoneReadBytes 10000000 --sjdbOverhang 100 --outFilterMultimapNmax 1
 
-## Deduplication: PICARD
-java -Xmx4g -jar picard.jar MarkDuplicates I=./star/${sample_prefix}.bam O=./star/${sample_prefix}_nodup.bam REMOVE_DUPLICATES=true VALIDATION_STRINGENCY=LENIENT
-
-## Identify ribosomal reads: samtools + bbtools + silva db
-samtools bam2fq ./star/${sample_prefix}_nodup.bam >./star/${sample_prefix}_nodup.bam.fastq
-bbduk.sh in=./star/${sample_prefix}_nodup.bam.fastq out=./star/${sample_prefix}_nodup_norrna.bam.fastq outm=./star/${sample_prefix}_nodup_rrna.bam.fastq ref=/mnt/flatfiles/rrna_silva/ribokmers.fa k=31 hdist=0 stats=./star/${sample_prefix}_rrnacheck_stats_detail.txt -eoom threads=16
-cat ./star/${sample_prefix}_nodup_norrna.bam.fastq | sed -n '1~4s/^@/>/p;2~4p' | grep ">" | sed 's/>//g' | sed 's/\/1$//g' | sed 's/\/2$//g' | awk '!a[$0]++' >./star/${sample_prefix}_nodup.bam.rrna.txt
-
-## Identify chrM ids: samtools
-echo -e "chrM" | xargs --max-procs=8 samtools view ./star/${sample_prefix}_nodup.bam | cut -f1 | awk '!a[$0]++' >./star/${sample_prefix}_nodup.bam.chrm.txt
-
-## Remove rRNA and chrM reads: PICARD
-cat ./star/${sample_prefix}_nodup.bam.rrna.txt >./star/${sample_prefix}_nodup.bam.remove.txt
-cat ./star/${sample_prefix}_nodup.bam.chrm.txt >>./star/${sample_prefix}_nodup.bam.remove.txt
-java -Xmx24g -jar picard.jar FilterSamReads I=./star/${sample_prefix}_nodup.bam O=./star/${sample_prefix}_nodup_filter.bam READ_LIST_FILE=./star/${sample_prefix}_nodup.bam.remove.txt FILTER=excludeReadList
-
 ## Generate BigWig normalized to mapped reads per sample: python package deeptools 3.5.1 / bamCoverage
 bamCoverage -b ./star/${sample_prefix}_nodup_filter.bam -o ./star/${sample_prefix}_nodup_filter.bw -p 16 --binSize 25 --smoothLength 75 --normalizeUsing RPKM --outFileFormat bigwig
 
 ## Count reads per genes: subread featureCounts
 featureCounts -T 16 -o ./star/${sample_prefix}_fcounts.txt -t exon -g gene_id -s 1 -a mus_musculus.101.mainChr.gtf ./star/${sample_prefix}_nodup_filter.bam
-
 
 ##
 ## Combine per-sample results
