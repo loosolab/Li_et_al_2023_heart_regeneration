4f7b7afcace24c1eb5bdf50a57abe7764409fee2
ckuenne <ckuenne@users.noreply.github.com>
2023-04-13 13:49:23 +0200

    Update RNA_base_processing.sh

--- RNA/RNA_base_processing.sh
+++ RNA/RNA_base_processing.sh
@@ -1,12 +1,19 @@
-## Reference GTF/FASTA: 
-Ensembl mus musculus release 101 (mm10)
+## Reference GTF/FASTA: Ensembl mus musculus release 101 (mm10)
+## place fastq files in subfolder ./raw/
+## prepare STAR genome index as described in manual
 
-## Samples:
+## Example for samples (different per dataset):
 prefix	fastq
 ctrl_1	ctrl_1_R1.fastq
 ctrl_2	ctrl_2_R1.fastq
-ko-tet3_1	ko-tet3_1_R1.fastq
-ko-tet3_2	ko-tet3_2_R1.fastq
+ctrl_3	ctrl_3_R1.fastq
+ko_1	ko_1_R1.fastq
+ko_2	ko_2_R1.fastq
+ko_3	ko_3_R1.fastq
+
+##
+## Repeat the following steps per sample
+##
 
 ## Trimming: Trimmomatic
 java -jar /mnt/software/x86_64/packages/trimmomatic/0.39/trimmomatic-0.39.jar SE -threads 8 ./raw/${sample_prefix}_R1.fastq ./trim/${sample_prefix}_R1_trim.fastq HEADCROP:0 LEADING:0 TRAILING:0 SLIDINGWINDOW:5:15 CROP:500 AVGQUAL:0 MINLEN:15
@@ -36,10 +43,16 @@ java -Xmx24g -jar /mnt/software/x86_64/packages/picard/2.21.7/picard.jar FilterS
 ## Count reads per genes: subread featureCounts
 /mnt/software/x86_64/packages/subread/1.6.5/bin/featureCounts -T 16 -o ./star/${sample_prefix}_fcounts.txt -t exon -g gene_id -s 1 -a mus_musculus.101.mainChr.gtf ./star/${sample_prefix}_nodup_filter.bam
 
+
+##
+## Combine per-sample results
+##
+
 ## Join all sample counts to raw count matrix: =counts.matrix
 merge_counts.pl ./star/*_fcounts.txt >counts.matrix
 
 ## RNA differential analysis: R with DESeq2 1.26.0 package
+## de_analysis.R script has to be adapted to include samples/conditions of the respective dataset
 R --vanilla -q < de_analysis.R
 
 ## Normalize matrix for downstream plots: R with DESeq2 1.26.0 package, =counts.matrix.norm
